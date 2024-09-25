2129b605efcf6a80938f74184617e0dbf10af8f8
Chris Wiley <sithlord2k@gmail.com>
2024-09-25 16:09:19 -0400

    Revert "Add project files."
    This reverts commit e1d98aabc2346c3e286a8de3e1bc16c0614bc6ee.

--- WPLBlazor/Models/TeamDetails.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WPLBlazor.Models
-{
-    public class TeamDetails
-    {
-        public int Id { get; set; }
-        public string TeamName { get; set; } = string.Empty;
-        public string Captain { get; set; } = string.Empty;
-    }
-}
--- WPLStatsCoreWebAPI/Models/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WPLBlazor.API.Models;
-
-public partial class Player
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public int TeamId { get; set; }
-}
